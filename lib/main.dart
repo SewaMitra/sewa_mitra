5615c771bf4a0882089f42a5366f5f9fcaacdc4f
salmanansari021 <021theyaanbrutal@gmail.com>
2026-06-03 20:29:41 +0545

    update

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: 'Sewa Mitra',
       theme: ThemeData(
         primaryColor: const Color(0xFFFF6B35),
