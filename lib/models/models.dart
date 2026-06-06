bbf53839b5cae5e8f89a9e64f9b04c1a45f4b28b
prajanya665 <prajanya7singh@gmail.com>
2026-06-06 22:51:38 +0545

    Add Booking model and BookingData storage

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -31,3 +31,33 @@ class ServiceProvider {
     required this.specialty,
   });
 }
+
+class Booking {
+  final String id;
+  final String serviceName;
+  final String providerName;
+  final String date;
+  final String time;
+  final String address;
+  final double amount;
+  final String status;
+
+  Booking({
+    required this.id,
+    required this.serviceName,
+    required this.providerName,
+    required this.date,
+    required this.time,
+    required this.address,
+    required this.amount,
+    this.status = 'Confirmed',
+  });
+}
+
+class BookingData {
+  static final List<Booking> bookings = [];
+
+  static void addBooking(Booking booking) {
+    bookings.add(booking);
+  }
+}
