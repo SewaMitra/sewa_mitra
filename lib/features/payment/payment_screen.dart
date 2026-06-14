bc9eb788f78ef83aa7d256226ead2393a7ef8988
salmanansari021 <021theyaanbrutal@gmail.com>
2026-06-14 19:58:16 +0545

    update

--- lib/features/payment/payment_screen.dart
+++ lib/features/payment/payment_screen.dart
@@ -103,7 +103,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
         ),
         backgroundColor: Colors.white,
         elevation: 0,
-        centerTitle: false,
+        centerTitle: true,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
