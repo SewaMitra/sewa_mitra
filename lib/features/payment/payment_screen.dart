f123200413a9043a4508ad3554498a9bb44cf8ef
salmanansari021 <021theyaanbrutal@gmail.com>
2026-06-14 19:49:12 +0545

    new change

--- lib/features/payment/payment_screen.dart
+++ lib/features/payment/payment_screen.dart
@@ -288,7 +288,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
             Icon(
               icon,
               size: 28,
-              color: primaryOrange,  // All icons always orangecu
+              color: primaryOrange,  // All icons always orange
             ),
             const SizedBox(width: 16),
             Expanded(
